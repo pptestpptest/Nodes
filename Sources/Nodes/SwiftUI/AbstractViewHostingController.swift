f8b59e9f8cfbd1080a1b4217273d0cc6d04ac532
Christopher Fuller <christopher.fuller@gotinder.com>
2021-06-03 18:37:00 -0700

    Add SwiftUI types (#52)

---
+++ Sources/Nodes/SwiftUI/AbstractViewHostingController.swift
@@ -0,0 +1,41 @@
+//
+//  AbstractViewHostingController.swift
+//  Nodes
+//
+//  Created by Christopher Fuller on 5/11/21.
+//
+
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+@available(macOS 10.15, macCatalyst 13.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public protocol NodesView: View {
+
+    associatedtype ReceiverType
+
+    var receiver: ReceiverType { get set }
+}
+
+@available(macOS 10.15, macCatalyst 13.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public protocol NodesViewHostingController {
+
+    associatedtype ContentType: NodesView
+
+    var rootView: ContentType { get }
+}
+
+#if canImport(UIKit) && !os(watchOS)
+
+@available(macCatalyst 13.0, iOS 13.0, tvOS 13.0, *)
+open class AbstractViewHostingController<View: NodesView>: UIHostingController<View>, NodesViewHostingController {
+
+    public var receiver: View.ReceiverType {
+        get { rootView.receiver }
+        set { rootView.receiver = newValue }
+    }
+}
+
+#endif
+
+#endif
---
+++ Sources/Nodes/SwiftUI/WithViewState.swift
@@ -0,0 +1,34 @@
+//
+//  WithViewState.swift
+//  Nodes
+//
+//  Created by Christopher Fuller on 5/11/21.
+//
+
+#if canImport(Combine) && canImport(SwiftUI)
+
+import Combine
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, macCatalyst 13, tvOS 13.0, watchOS 6.0, *)
+public struct WithViewState<ViewState: InitialStateProviding, Content: View>: View {
+
+    public var body: some View {
+        content(viewState).onReceive(publisher) { viewState = $0 }
+    }
+
+    private let publisher: AnyPublisher<ViewState, Never>
+    private let content: (ViewState) -> Content
+
+    @State(initialValue: .initialState) private var viewState: ViewState
+
+    public init<P: Publisher>(
+        _ publisher: P,
+        @ViewBuilder content: @escaping (ViewState) -> Content
+    ) where P.Output == ViewState, P.Failure == Never {
+        self.publisher = publisher.eraseToAnyPublisher()
+        self.content = content
+    }
+}
+
+#endif
