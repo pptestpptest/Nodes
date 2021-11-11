9e04590f30dfb17f75eb2824a68fe3a750f2da44
Christopher Fuller <christopher.fuller@gotinder.com>
2021-11-10 17:26:49 -0800

    Refactor XcodeTemplateGeneratorCommand (#80)

--- Sources/XcodeTemplateGeneratorTool/XcodeTemplateGeneratorCommand.swift
+++ Sources/XcodeTemplateGeneratorTool/XcodeTemplateGeneratorCommand.swift
@@ -11,15 +11,17 @@ import XcodeTemplateGeneratorLibrary
 @main
 internal struct XcodeTemplateGeneratorCommand: ParsableCommand {
 
-    internal static var configuration: CommandConfiguration = .init(commandName: "xc-template-generator",
+    internal static let configuration: CommandConfiguration = .init(commandName: "xc-template-generator",
                                                                     abstract: "Xcode Template Generator")
 
-    // The Xcode templates identifier
-    @Option(name: .customLong("id"), help: "The Xcode templates identifier.")
+    @Option(name: .customLong("id"),
+            help: "The Xcode templates identifier.")
+
     private var identifier: String
 
-    // The YAML config file path. (optional)
-    @Option(name: .customLong("config"), help: "The YAML config file path. (optional)")
+    @Option(name: .customLong("config"),
+            help: "The YAML config file path. (optional)")
+
     private var path: String?
 
     internal func run() throws {
