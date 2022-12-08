f189e08c3a000b1081a449c5957393e2b0855279
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2022-12-08 15:44:19 -0800

    Introduce StencilTemplate (#225)
    * Introduce StencilTemplate
    
    * Add additional docs
    
    * Improve documentation
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Improve tests
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Remove cyclomatic complexity
    
    * Improve filename switch statement
    
    * Improve documentation
    
    * Fix formatting
    
    * Fix documentation formatting
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Improve forEach usage
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Improve CustomStringConvertible usage
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Fix
    
    * Improve doc comment
    
    * Improve doc comment
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

---
+++ Sources/XcodeTemplateGeneratorLibrary/StencilTemplate.swift
@@ -0,0 +1,109 @@
+//
+//  StencilTemplate.swift
+//  XcodeTemplateGeneratorLibrary
+//
+//  Created by Garric Nahapetian on 11/7/22.
+//
+
+import Foundation
+
+/// Every Stencil source file is represented by a case. Some cases have a variation.
+public enum StencilTemplate: Equatable, CaseIterable, CustomStringConvertible {
+
+    case analytics
+    case builder(Variation)
+    case context
+    case flow
+    case plugin
+    case pluginList
+    case viewController(Variation)
+    case worker
+
+    /// Alternate Stencil source files for specific use cases.
+    public enum Variation: String, Equatable, CaseIterable {
+
+        case `default` = ""
+        case swiftUI = "-SwiftUI"
+
+        internal static func variation(for kind: UIFramework.Kind) -> Self {
+            kind == .swiftUI ? .swiftUI : .default
+        }
+    }
+
+    /// An array of StencilTemplate cases for ``CaseIterable`` conformance.
+    public static let allCases: [StencilTemplate] = [
+        .analytics,
+        .builder(.default),
+        .builder(.swiftUI),
+        .context,
+        .flow,
+        .plugin,
+        .pluginList,
+        .viewController(.default),
+        .viewController(.swiftUI),
+        .worker
+    ]
+
+    /// The name of the Stencil template.
+    public var description: String {
+        switch self {
+        case .analytics:
+            return "Analytics"
+        case .builder:
+            return "Builder"
+        case .context:
+            return "Context"
+        case .flow:
+            return "Flow"
+        case .plugin:
+            return "Plugin"
+        case .pluginList:
+            return "PluginList"
+        case .viewController:
+            return "ViewController"
+        case .worker:
+            return "Worker"
+        }
+    }
+
+    /// The name of the Stencil source file in the XcodeTemplateGeneratorLibrary bundle.
+    public var filename: String {
+        switch self {
+        case .analytics, .context, .flow, .plugin, .pluginList, .worker:
+            return description
+        case let .builder(variation), let .viewController(variation):
+            return description.appending(variation.rawValue)
+        }
+    }
+
+    /// The StencilTemplate cases that represent a Node.
+    ///
+    /// - Parameters:
+    ///   - variation: The Stencil variation.
+    ///   - withViewController: A Boolean indicating whether or not to include the `viewController` stencil.
+    ///
+    /// - Returns: An array of StencilTemplate cases.
+    public static func nodeStencils(
+        for variation: Variation = .default,
+        withViewController isViewControllerIncluded: Bool = true
+    ) -> [StencilTemplate] {
+        if isViewControllerIncluded {
+            return [
+                .analytics,
+                .builder(variation),
+                .context,
+                .flow,
+                .viewController(variation),
+                .worker
+            ]
+        } else {
+            return [
+                .analytics,
+                .builder(variation),
+                .context,
+                .flow,
+                .worker
+            ]
+        }
+    }
+}
---
+++ Tests/XcodeTemplateGeneratorLibraryTests/StencilTemplateTests.swift
@@ -0,0 +1,126 @@
+//
+//  StencilTemplateTests.swift
+//  XcodeTemplateGeneratorLibraryTests
+//
+//  Created by Garric Nahapetian on 12/6/22.
+//
+
+import Nimble
+@testable import XcodeTemplateGeneratorLibrary
+import XCTest
+
+final class StencilTemplateTests: XCTestCase {
+
+    func testVariationRawValue() {
+        StencilTemplate.Variation.allCases.forEach { variation in
+            switch variation {
+            case .default:
+                expect(variation.rawValue) == ""
+            case .swiftUI:
+                expect(variation.rawValue) == "-SwiftUI"
+            }
+        }
+    }
+
+    func testVariationForKind() {
+        UIFramework.Kind.allCases.forEach { kind in
+            let variation: StencilTemplate.Variation = .variation(for: kind)
+            switch kind {
+            case .appKit, .uiKit, .custom:
+                expect(variation) == .default
+            case .swiftUI:
+                expect(variation) == .swiftUI
+            }
+        }
+    }
+
+    func testAllCases() {
+        expect(StencilTemplate.allCases) == [
+            .analytics,
+            .builder(.default),
+            .builder(.swiftUI),
+            .context,
+            .flow,
+            .plugin,
+            .pluginList,
+            .viewController(.default),
+            .viewController(.swiftUI),
+            .worker
+        ]
+    }
+
+    func testDescription() {
+        StencilTemplate.allCases.forEach { stencilTemplate in
+            let description: String = "\(stencilTemplate)"
+            switch stencilTemplate {
+            case .analytics:
+                expect(description) == "Analytics"
+            case .builder:
+                expect(description) == "Builder"
+            case .context:
+                expect(description) == "Context"
+            case .flow:
+                expect(description) == "Flow"
+            case .plugin:
+                expect(description) == "Plugin"
+            case .pluginList:
+                expect(description) == "PluginList"
+            case .viewController:
+                expect(description) == "ViewController"
+            case .worker:
+                expect(description) == "Worker"
+            }
+        }
+    }
+
+    func testFilename() {
+        StencilTemplate.allCases.forEach { stencilTemplate in
+            let filename: String = stencilTemplate.filename
+            switch stencilTemplate {
+            case .analytics:
+                expect(filename) == "Analytics"
+            case let .builder(variation):
+                expect(filename) == "Builder\(variation == .swiftUI ? "-SwiftUI" : "")"
+            case .context:
+                expect(filename) == "Context"
+            case .flow:
+                expect(filename) == "Flow"
+            case .plugin:
+                expect(filename) == "Plugin"
+            case .pluginList:
+                expect(filename) == "PluginList"
+            case let .viewController(variation):
+                expect(filename) == "ViewController\(variation == .swiftUI ? "-SwiftUI" : "")"
+            case .worker:
+                expect(filename) == "Worker"
+            }
+        }
+    }
+
+    func testNodeStencils() {
+        StencilTemplate.Variation.allCases.forEach { variation in
+            [true, false].forEach { withViewController in
+                let stencils: [StencilTemplate] = StencilTemplate.nodeStencils(for: variation,
+                                                                               withViewController: withViewController)
+                if withViewController {
+                    expect(stencils) == [
+                        .analytics,
+                        .builder(variation),
+                        .context,
+                        .flow,
+                        .viewController(variation),
+                        .worker
+                    ]
+                } else {
+                    expect(stencils) == [
+                        .analytics,
+                        .builder(variation),
+                        .context,
+                        .flow,
+                        .worker
+                    ]
+                }
+            }
+        }
+    }
+}
