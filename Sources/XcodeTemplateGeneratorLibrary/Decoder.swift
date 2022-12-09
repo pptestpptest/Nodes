2d399315a96343dfd78ae8e0142017a47e53fc01
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2022-12-09 12:52:58 -0800

    Remove decodeString as not used (#236)

--- Sources/XcodeTemplateGeneratorLibrary/Decoder.swift
+++ Sources/XcodeTemplateGeneratorLibrary/Decoder.swift
@@ -8,10 +8,6 @@
 // Workaround for https://github.com/jpsim/Yams/issues/301
 extension Decoder {
 
-    internal func decodeString(_ key: String) throws -> String {
-        try decode(key, as: String.self)
-    }
-
     internal func decodeString<K: CodingKey>(_ key: K) throws -> String {
         try decode(key, as: String.self)
     }
