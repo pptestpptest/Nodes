924fcc83434db8be379d9d649e0a3ca67bbe9fa7
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2022-12-15 16:23:53 -0800

    Add imports to NodeViewInjectedContext (#248)
    * Add imports to NodeViewInjectedContext
    
    * Re-record snapshots after merge conflct

--- Sources/XcodeTemplateGeneratorLibrary/Templates/Contexts/NodeViewInjectedContext.swift
+++ Sources/XcodeTemplateGeneratorLibrary/Templates/Contexts/NodeViewInjectedContext.swift
@@ -10,9 +10,11 @@ public struct NodeViewInjectedContext: Context {
     private let fileHeader: String
     private let nodeName: String
     private let workerName: String
+    private let analyticsImports: [String]
     private let builderImports: [String]
     private let contextImports: [String]
     private let flowImports: [String]
+    private let stateImports: [String]
     private let workerImports: [String]
     private let dependencies: [[String: Any]]
     private let flowProperties: [[String: Any]]
@@ -27,9 +29,11 @@ public struct NodeViewInjectedContext: Context {
             "worker_name": workerName,
             "owns_view": false,
             "root_node": false,
+            "analytics_imports": analyticsImports,
             "builder_imports": builderImports,
             "context_imports": contextImports,
             "flow_imports": flowImports,
+            "state_imports": stateImports,
             "worker_imports": workerImports,
             "dependencies": dependencies,
             "flow_properties": flowProperties,
@@ -43,9 +47,11 @@ public struct NodeViewInjectedContext: Context {
         fileHeader: String,
         nodeName: String,
         workerName: String,
+        analyticsImports: Set<String>,
         builderImports: Set<String>,
         contextImports: Set<String>,
         flowImports: Set<String>,
+        stateImports: Set<String>,
         workerImports: Set<String>,
         dependencies: [XcodeTemplates.Variable],
         flowProperties: [XcodeTemplates.Variable],
@@ -56,9 +62,11 @@ public struct NodeViewInjectedContext: Context {
         self.fileHeader = fileHeader
         self.nodeName = nodeName
         self.workerName = workerName
+        self.analyticsImports = analyticsImports.sortedImports()
         self.builderImports = builderImports.sortedImports()
         self.contextImports = contextImports.sortedImports()
         self.flowImports = flowImports.sortedImports()
+        self.stateImports = stateImports.sortedImports()
         self.workerImports = workerImports.sortedImports()
         self.dependencies = dependencies.map(\.dictionary)
         self.flowProperties = flowProperties.map(\.dictionary)
