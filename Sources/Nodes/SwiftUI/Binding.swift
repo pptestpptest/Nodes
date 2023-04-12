bdfb9057d77788d232aa9dbb82c1ef9ec08a1899
Eman Haroutunian <104394983+tinder-emanharoutunian@users.noreply.github.com>
2023-04-12 16:09:47 -0700

    Update Binding documentation to reflect InitialStateProviding has been removed (#338)
    * Update Binding documentation
    
    * Update Sources/Nodes/SwiftUI/Binding.swift
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Update Sources/Nodes/SwiftUI/Binding.swift
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    ---------
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Sources/Nodes/SwiftUI/Binding.swift
+++ Sources/Nodes/SwiftUI/Binding.swift
@@ -21,7 +21,7 @@ extension Binding {
     /// Example:
     /// ```
     /// var body: some View {
-    ///     WithViewState(viewState) { viewState in
+    ///     WithViewState(initialState: initialState, statePublisher: statePublisher) { viewState in
     ///         Slider(value: .bind(to: viewState.sliderValue) { receiver?.didChangeSliderValue($0) },
     ///                in: 1...100)
     ///     }
@@ -46,7 +46,7 @@ extension Binding {
     /// Example:
     /// ```
     /// var body: some View {
-    ///     WithViewState(viewState) { viewState in
+    ///     WithViewState(initialState: initialState, statePublisher: statePublisher) { viewState in
     ///         Slider(value: .bind(to: viewState.sliderValue, onChange: receiver?.didChangeSliderValue),
     ///                in: 1...100)
     ///     }
