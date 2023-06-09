b3117a80db7a2c5edfce293e77d97f2ef06035fc
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-06-09 10:48:45 -0700

    Add XcodeTemplates.Variable mock helper (#394)
    * Add XcodeTemplates.Variable mock helper
    
    * Improve method signature and implementation

---
+++ Tests/NodesXcodeTemplatesGeneratorTests/Array.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+
+import NodesXcodeTemplatesGenerator
+
+extension Array where Element == XcodeTemplates.Variable {
+
+    internal static func mock(with identifier: String, count: Int) -> Self {
+        guard count > 0
+        else { return [] }
+        guard count > 1
+        else { return [XcodeTemplates.Variable(name: "<\(identifier)Name>", type: "<\(identifier)Type>")] }
+        return (1...count).map { count in
+            XcodeTemplates.Variable(name: "<\(identifier)Name\(count)>", type: "<\(identifier)Type\(count)>")
+        }
+    }
+}
