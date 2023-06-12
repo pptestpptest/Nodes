18cdfc66aaceecfdc0d7ceb182fe7ec2ddac31f6
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-06-12 10:18:59 -0700

    Refactor tests using range (#392)
    * Refactor tests using range
    
    * Rename
    
    * Improve name
    
    * Fix
    
    * Improve naming

--- Tests/NodesXcodeTemplatesGeneratorTests/StencilRendererTests.swift
+++ Tests/NodesXcodeTemplatesGeneratorTests/StencilRendererTests.swift
@@ -9,6 +9,8 @@ import XCTest
 
 final class StencilRendererTests: XCTestCase, TestFactories {
 
+    private let importCounts: ClosedRange<Int> = 0...2
+
     func testRenderNode() throws {
         try UIFramework.Kind.allCases.forEach { kind in
             let context: NodeContext = givenNodeContext()
@@ -63,7 +65,7 @@ final class StencilRendererTests: XCTestCase, TestFactories {
     }
 
     func testRenderPlugin() throws {
-        try (0...2).forEach { count in
+        try importCounts.forEach { count in
             let context: PluginContext = givenPluginContext(importCount: count)
             assertSnapshot(matching: try StencilRenderer().renderPlugin(context: context),
                            as: .lines,
@@ -72,7 +74,7 @@ final class StencilRendererTests: XCTestCase, TestFactories {
     }
 
     func testRenderPluginWithoutReturnType() throws {
-        try (0...2).forEach { count in
+        try importCounts.forEach { count in
             let context: PluginContext = givenPluginContextWithoutReturnType(importCount: count)
             assertSnapshot(matching: try StencilRenderer().renderPlugin(context: context),
                            as: .lines,
@@ -81,7 +83,7 @@ final class StencilRendererTests: XCTestCase, TestFactories {
     }
 
     func testRenderPluginList() throws {
-        try (0...2).forEach { count in
+        try importCounts.forEach { count in
             let context: PluginListContext = givenPluginListContext(importCount: count)
             assertSnapshot(matching: try StencilRenderer().renderPluginList(context: context),
                            as: .lines,
@@ -90,7 +92,7 @@ final class StencilRendererTests: XCTestCase, TestFactories {
     }
 
     func testRenderWorker() throws {
-        try (0...2).forEach { count in
+        try importCounts.forEach { count in
             let context: WorkerContext = givenWorkerContext(importCount: count)
             assertSnapshot(matching: try StencilRenderer().renderWorker(context: context),
                            as: .lines,
