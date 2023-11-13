cde4cd3fb210be081f80f598fd6e4a92d1b2b65b
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-11-13 16:13:34 -0700

    Shorten snapshot filenames (#491)
    * Shorten snapshot filenames
    
    * Use helper method
    
    * Improve test implementation
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Fix
    
    * Improve formatting and organization
    
    ---------
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Tests/NodesXcodeTemplatesGeneratorTests/XcodeTemplatesTests.swift
+++ Tests/NodesXcodeTemplatesGeneratorTests/XcodeTemplatesTests.swift
@@ -29,15 +29,7 @@ final class XcodeTemplatesTests: XCTestCase {
         try XcodeTemplates(config: Config()).generate(identifier: "identifier", using: fileSystem)
         // swiftlint:disable:next large_tuple
         let writes: [(contents: String, path: String, atomically: Bool)] = fileSystem.writes
-        writes.forEach { write in
-            let name: String = write.path
-                .split(separator: "/")
-                .reversed()[0...1]
-                .reversed()
-                .joined(separator: "-")
-                .replacingOccurrences(of: [".xctemplate", "___FILEBASENAME___", ".swift", ".plist"], with: "")
-            assertSnapshot(matching: write.contents, as: .lines, named: "Contents.\(name)")
-        }
+        writes.forEach { assertSnapshot(matching: $0.contents, as: .lines, named: "Contents.\(name(from: $0.path))") }
         assertSnapshot(matching: writes.map { (path: $0.path, atomically: $0.atomically) }, as: .dump, named: "Writes")
         assertSnapshot(matching: fileSystem.directories, as: .dump, named: "Directories")
         assertSnapshot(matching: fileSystem.copies, as: .dump, named: "Copies")
@@ -50,10 +42,19 @@ final class XcodeTemplatesTests: XCTestCase {
         try XcodeTemplates(config: Config()).generate(at: url, using: fileSystem)
         // swiftlint:disable:next large_tuple
         let writes: [(contents: String, path: String, atomically: Bool)] = fileSystem.writes
-        writes.forEach { assertSnapshot(matching: $0.contents, as: .lines, named: "Contents.\($0.path)") }
+        writes.forEach { assertSnapshot(matching: $0.contents, as: .lines, named: "Contents.\(name(from: $0.path))") }
         assertSnapshot(matching: writes.map { (path: $0.path, atomically: $0.atomically) }, as: .dump, named: "Writes")
         assertSnapshot(matching: fileSystem.directories, as: .dump, named: "Directories")
         assertSnapshot(matching: fileSystem.copies, as: .dump, named: "Copies")
         assertSnapshot(matching: fileSystem.deletions, as: .dump, named: "Deletions")
+    }
+
+    private func name(from path: String) -> String {
+        path
+            .split(separator: "/")
+            .reversed()[0...1]
+            .reversed()
+            .joined(separator: "-")
+            .replacingOccurrences(of: [".xctemplate", "___FILEBASENAME___", ".swift", ".plist"], with: "")
     }
 }
