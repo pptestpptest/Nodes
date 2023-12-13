46d663394bff6a9cf80502faae8fdfb26de641af
Christopher Fuller <christopher.fuller@gotinder.com>
2023-12-13 07:48:23 -0800

    Add WithViewState tests (#558)
    * Add WithViewState tests
    
    * Remove @testable

---
+++ Tests/NodesTests/SwiftUI/WithViewStateTests.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright © 2022 Tinder (Match Group, LLC)
+//
+
+import Combine
+import Nimble
+import Nodes
+import SwiftUI
+import XCTest
+
+@available(macOS 10.15, macCatalyst 13.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+final class WithViewStateTests: XCTestCase {
+
+    private struct State: Equatable {}
+
+    func testWithViewState() {
+        var states: [State] = []
+        let withViewState: WithViewState = .init(initialState: State(), statePublisher: Empty()) { state in
+            states.append(state)
+            return EmptyView()
+        }
+        expect(states).to(beEmpty())
+        expect(withViewState.body).to(beAKindOf(SubscriptionView<AnyPublisher<State, Never>, EmptyView>.self))
+        expect(states) == [State()]
+    }
+}
