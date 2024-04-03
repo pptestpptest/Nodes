077b105ddff46082fc5ecec8502e0e2f75f2c72c
Garric Nahapetian <garric.nahapetian@gotinder.com>
2024-04-03 11:04:48 -0700

    Expect to not throw for presets (#745)

--- Tests/NodesGeneratorTests/StencilContextsTests.swift
+++ Tests/NodesGeneratorTests/StencilContextsTests.swift
@@ -59,6 +59,9 @@ final class StencilContextsTests: XCTestCase, TestFactories {
             .to(throwError(errorType: StencilContextError.self) { error in
                 expect(error) == .invalidPreset("Window")
             })
+        expect { try self.givenNodeStencilContext(preset: .root) }
+            .toNot(throwError())
+
     }
 
     func testNodeViewInjectedStencilContext() throws {
@@ -86,6 +89,12 @@ final class StencilContextsTests: XCTestCase, TestFactories {
     }
 
     func testNodeViewInjectedStencilContextThrowsInvalidPreset() {
+        expect { try self.givenNodeViewInjectedStencilContext(preset: .app) }
+            .toNot(throwError())
+        expect { try self.givenNodeViewInjectedStencilContext(preset: .scene) }
+            .toNot(throwError())
+        expect { try self.givenNodeViewInjectedStencilContext(preset: .window) }
+            .toNot(throwError())
         expect { try self.givenNodeViewInjectedStencilContext(preset: .root) }
             .to(throwError(errorType: StencilContextError.self) { error in
                 expect(error) == .invalidPreset("Root")
